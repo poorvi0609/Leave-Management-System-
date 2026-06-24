bdf08792c57c3d930a7ae3bd2f89cfd053ef152f
poorvi0609 <agarwalpoorvi9@gmail.com>
2026-06-24 15:08:14 +0530

    feat: add repositories for all entities

---
+++ lms/src/main/java/com/lms/auth/RefreshTokenRepository.java
@@ -0,0 +1,17 @@
+package com.lms.auth;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByEmployeeId(Long employeeId);
+
+    void deleteByToken(String token);
+
+    boolean existsByTokenAndIsRevokedFalse(String token);
+}
